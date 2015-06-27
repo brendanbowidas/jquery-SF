068ce4cb8dbc14a046f28a4736c61b5d86f2d35c
brendanbowidas <brendanbowidas@gmail.com>
2015-06-27 17:24:10 -0400

    transferred from old repo

---
+++ js/app.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    
+	$(document).keydown(function(key) {
+		if(key.which==88) {
+			
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-cool').show();
+		}
+		
+	});
+
+	$('.ryu').mouseenter(function() {
+          $('.ryu-still').hide();
+          $('.ryu-cool').hide();
+          $('.ryu-ready').show();
+
+	})
+
+	.mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+		$('.ryu-cool').hide();
+	})
+
+	.mousedown(function() {
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-cool').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'}, 500, 
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+			);
+		
+	})
+
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-cool').hide();
+		$('.ryu-ready').show();
+	});
+	
+	
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
